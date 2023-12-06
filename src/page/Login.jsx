1aab43ad92e61f4aa7b476eb5cd227262e66a57e
BrandonJ21 <bjeneralreyes@gmail.com>
2023-12-06 16:47:46 -0300

    Login

---
+++ src/page/Login.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+export const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = form;
+
+  const onPress = () => {
+    console.log({ form });
+  };
+
+  const onChange = (value, field) => {
+    setForm({
+      ...form,
+      [field]: value,
+    });
+  };
+
+  return (
+    <View style={styles.continue}>
+      <View style={styles.card}>
+        <Text style={{...styles.textCard, fontSize:40}}>Micro App</Text>
+        <View>
+          <Text style={styles.textCard}>Correo:</Text>
+
+          <TextInput
+            style={styles.textInput}
+            placeholder="Correo electrónico"
+            placeholderTextColor={"#0e4e9c"}
+            value={email}
+            onChangeText={(value) => onChange(value, "email")}
+            keyboardType="email-address"
+          />
+        </View>
+        <View>
+          <Text style={styles.textCard}>Contraseña:</Text>
+          <TextInput secureTextEntry 
+            style={styles.textInput}
+            placeholder="Contraseña"
+            placeholderTextColor={"#0e4e9c"}
+            autoCorrect={false}
+            value={password}
+            onChangeText={(value) => onChange(value, "password")}
+            keyboardType="visible-password"
+          />
+        </View>
+        <View>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={onPress}
+            style={styles.button}
+          >
+            <Text style={styles.textButton}>Iniciar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  continue: {
+    flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  card: {
+    width: "90%",
+    height: 550,
+    backgroundColor: "#125098",
+    borderRadius: 15,
+    padding: 20,
+    marginTop: '30%',
+    margin: 20,
+  },
+  textCard: {
+    fontSize: 20,
+    color: "white",
+    textAlign: "center",
+    marginTop: 40,
+  },
+  textInput: {
+    fontSize: 20,
+    color: "black",
+    textAlign: "center",
+    marginTop: 20,
+    backgroundColor: "white",
+    borderRadius: 15,
+    height: 50,
+  },
+  button: {
+    marginTop: 50,
+    margin: 10,
+    padding: 15,
+    backgroundColor: "white",
+    borderRadius: 15,
+  },
+  textButton: {
+    textAlign: "center",
+    fontSize: 19,
+    fontWeight: "bold",
+    color: "#0c4e9c",
+  },
+});
